c8a3eb09fad0ea01df2fa110d40ca1f69cc7e0d1
elmaester <olegmorket@gmail.com>
2022-01-07 23:09:34 +0200

    added styles, fonts, posts list page and its logic, removed junk

--- src/pages/{MarkdownRemark.fields__slug}.js
+++ src/pages/{MarkdownRemark.fields__slug}.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { graphql } from "gatsby";
-import "../styles.scss";
+import "../styles/main.scss";
 
 export default function Template({ data }) {
   const { frontmatter, html } = data.markdownRemark;
   return (
-    <main className="blog-post-container">
+    <main className="central-container">
       <article className="blog-post">
         <h1 className="blog-post-title">{frontmatter.title}</h1>
-        <time className="blog-post-date">Written {frontmatter.date}</time>
+        <time className="blog-post-date">Published on {frontmatter.date}</time>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
@@ -17,12 +17,15 @@ export default function Template({ data }) {
     </main>
   );
 }
+
+// FrontMatter: title, date, tags, related
+
 export const pageQuery = graphql`
   query ($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
-        date(fromNow: true)
+        date(formatString: "MMMM DD, YYYY")
         title
       }
     }
