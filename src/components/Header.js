f5abf78ba64d6edd3566f192ad968cf0b28094fb
elmaester <olegmorket@gmail.com>
2022-01-12 23:40:05 +0200

    add telegram icon link, sticky header, style changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import { Link } from "gatsby";
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import RssLogo from "../images/rss.png";
+import TelegramLogo from "../images/telegram.png";
 
 const Header = () => {
   const data = useStaticQuery(graphql`
@@ -19,6 +20,9 @@ const Header = () => {
         <Link to="/" className="header-homepage-link">
           {data.site.siteMetadata.title}
         </Link>
+        <a href="https://t.me/+ExSBktJktPU5NzQy" className="header-first-icon">
+          <img src={TelegramLogo} alt="telegram" />
+        </a>
         <a href="/rss.xml">
           <img src={RssLogo} alt="rss" />
         </a>
