311695b8b4658bb8d8ed7c52a5cb163c52525981
elmaester <olegmorket@gmail.com>
2022-01-18 12:03:57 +0200

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,41 @@
 import * as React from "react";
+import { graphql, Link } from "gatsby";
+import { Helmet } from "react-helmet";
+import Header from "../components/Header";
 
-const NotFoundPage = () => {
-  return <></>;
+const NotFoundPage = ({ data }) => {
+  return (
+    <>
+      <Helmet>
+        <title>Not found - {data.site.siteMetadata.title}</title>
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      </Helmet>
+      <Header />
+      <main className="set-global-width blog-post">
+        <h1>Page not found</h1>
+        <p>
+          Sorry{" "}
+          <span role="img" aria-label="Pensive emoji">
+            😔
+          </span>{" "}
+          We couldn’t find what you were looking for.
+        </p>
+        <br />
+        <Link to="/">Go home</Link>
+      </main>
+    </>
+  );
 };
 
 export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
