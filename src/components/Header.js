0bca1dcb80d42a9a8f2dd25c6046a8321e54e5ea
elmaester <olegmorket@gmail.com>
2022-02-04 08:24:05 +0200

    add twitter icon link, remove unused icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import RssLogo from "../images/rss.png";
 import TelegramLogo from "../images/telegram.png";
+import TwitterLogo from "../images/twitter.png";
 
 const Header = () => {
   const data = useStaticQuery(graphql`
@@ -20,10 +21,13 @@ const Header = () => {
         <Link to="/" className="header-homepage-link">
           {data.site.siteMetadata.title}
         </Link>
-        <a href="https://t.me/+ExSBktJktPU5NzQy" className="header-first-icon">
+        <a href="https://twitter.com/oleg_date" className="header-social-link-first">
+          <img src={TwitterLogo} alt="twitter" width="36" height="36" />
+        </a>
+        <a href="https://t.me/+ExSBktJktPU5NzQy" className="header-social-link">
           <img src={TelegramLogo} alt="telegram" width="36" height="36" />
         </a>
-        <a href="/rss">
+        <a href="/rss" className="header-social-link">
           <img src={RssLogo} alt="rss" width="36" height="36" />
         </a>
       </div>
